82da576b8f2684e3640dcadf7daedc97cb98a6e9
chunnusharma <100778124+chunnusharma@users.noreply.github.com>
2022-05-26 19:26:02 +0530

    Create merge of two list .py

---
+++ merge of two list .py
@@ -0,0 +1,14 @@
+def merge(*lst):
+    lst=[]
+    # lst=lst1+lst2
+    # OR
+    for i in lst1:
+        lst.append(i)
+    for i in lst2:
+        lst.append(i)
+    print(lst)
+lst1=[]
+lst2=[]
+lst1=list(map(eval,input().split()))
+lst2=list(map(eval,input().split()))
+merge(lst1,lst2)"""
