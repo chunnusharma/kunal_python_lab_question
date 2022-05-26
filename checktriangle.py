86dfdb653d304f1b923c76c5dbcc1ff05c6b917d
chunnusharma <100778124+chunnusharma@users.noreply.github.com>
2022-05-26 19:54:39 +0530

    Create checktriangle.py

---
+++ checktriangle.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the first side"))
+n2=int(input("Enter the second side"))
+n3=int(input("Enter the third side"))
+
+if n1==n2==n3:
+    print("The triangle is equilateral")
+elif n1!=n2!=n1:
+    print("The triangle is scalene")
+else:
+    print("The triangle is isoscales")
